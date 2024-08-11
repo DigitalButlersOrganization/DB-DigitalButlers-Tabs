aab7d8e068ef06c8ae865e12b78051b104170b26
Sasha Afanasienka <afanasienka@yandex.by>
2024-08-11 13:08:32 +0300

    fix: add wxport form main class

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-import { Tabs } from '../../dist/index';
+export {
+	Tabs,
+} from './dist/index.js';
 
-const wrappers = document.querySelectorAll('.wrapper');
+// import { Tabs } from '../../dist/index';
 
-wrappers.forEach((wrapper) => {
-	// eslint-disable-next-line no-unused-vars
-	const tabs = new Tabs(wrapper, {
-		tabpanelsListSelector: '[data-tabs="content"]',
-		tabbuttonsListSelector: '[data-tabs="tabs"]',
-		deletableTabs: true,
-		triggerEvent: 'mouseover',
-	});
-});
+// const wrappers = document.querySelectorAll('.wrapper');
 
-// eslint-disable-next-line no-unused-vars
-const mainTabs = new Tabs('.app', {
-	tabpanelsListSelector: '.content',
-	tabbuttonsListSelector: '.app-tabs',
-	orientation: 'vertical',
-	deletableTabs: true,
-	animation: {
-		delay: 1000,
-	},
-	matchMediaRule: '(min-width: 600px)',
-	autoplay: {
-		delay: 0,
-	},
-	on: {
-		tabChange: (tabs) => {
-			console.log(tabs.activeIndex);
-		},
-	},
-});
+// wrappers.forEach((wrapper) => {
+// 	// eslint-disable-next-line no-unused-vars
+// 	const tabs = new Tabs(wrapper, {
+// 		tabpanelsListSelector: '[data-tabs="content"]',
+// 		tabbuttonsListSelector: '[data-tabs="tabs"]',
+// 		deletableTabs: true,
+// 		triggerEvent: 'mouseover',
+// 	});
+// });
+
+// // eslint-disable-next-line no-unused-vars
+// const mainTabs = new Tabs('.app', {
+// 	tabpanelsListSelector: '.content',
+// 	tabbuttonsListSelector: '.app-tabs',
+// 	orientation: 'vertical',
+// 	deletableTabs: true,
+// 	animation: {
+// 		delay: 1000,
+// 	},
+// 	matchMediaRule: '(min-width: 600px)',
+// 	autoplay: {
+// 		delay: 0,
+// 	},
+// 	on: {
+// 		tabChange: (tabs) => {
+// 			console.log(tabs.activeIndex);
+// 		},
+// 	},
+// });
