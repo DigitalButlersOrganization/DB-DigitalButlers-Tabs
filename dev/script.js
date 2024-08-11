951dc7246c57a1bf620a3fbe84ac4cfc29c54333
Sasha Afanasienka <afanasienka@yandex.by>
2024-08-11 13:50:30 +0300

    feat: remove unused dependencies

---
+++ dev/script.js
@@ -0,0 +1,33 @@
+import { Tabs } from '../dist/index';
+
+const wrappers = document.querySelectorAll('.wrapper');
+
+wrappers.forEach((wrapper) => {
+	// eslint-disable-next-line no-unused-vars
+	const tabs = new Tabs(wrapper, {
+		tabpanelsListSelector: '[data-tabs="content"]',
+		tabbuttonsListSelector: '[data-tabs="tabs"]',
+		deletableTabs: true,
+		triggerEvent: 'mouseover',
+	});
+});
+
+// eslint-disable-next-line no-unused-vars
+const mainTabs = new Tabs('.app', {
+	tabpanelsListSelector: '.app-panels',
+	tabbuttonsListSelector: '.app-tabs',
+	orientation: 'vertical',
+	deletableTabs: true,
+	animation: {
+		delay: 1000,
+	},
+	matchMediaRule: '(min-width: 600px)',
+	autoplay: {
+		delay: 0,
+	},
+	on: {
+		tabChange: (tabs) => {
+			console.log(tabs.activeIndex);
+		},
+	},
+});
--- index.js
+++ index.js
@@ -1,35 +1,3 @@
 export * from './dist/index.js';
 
-// import { Tabs } from '../../dist/index';
 
-// const wrappers = document.querySelectorAll('.wrapper');
-
-// wrappers.forEach((wrapper) => {
-// 	// eslint-disable-next-line no-unused-vars
-// 	const tabs = new Tabs(wrapper, {
-// 		tabpanelsListSelector: '[data-tabs="content"]',
-// 		tabbuttonsListSelector: '[data-tabs="tabs"]',
-// 		deletableTabs: true,
-// 		triggerEvent: 'mouseover',
-// 	});
-// });
-
-// // eslint-disable-next-line no-unused-vars
-// const mainTabs = new Tabs('.app', {
-// 	tabpanelsListSelector: '.content',
-// 	tabbuttonsListSelector: '.app-tabs',
-// 	orientation: 'vertical',
-// 	deletableTabs: true,
-// 	animation: {
-// 		delay: 1000,
-// 	},
-// 	matchMediaRule: '(min-width: 600px)',
-// 	autoplay: {
-// 		delay: 0,
-// 	},
-// 	on: {
-// 		tabChange: (tabs) => {
-// 			console.log(tabs.activeIndex);
-// 		},
-// 	},
-// });
