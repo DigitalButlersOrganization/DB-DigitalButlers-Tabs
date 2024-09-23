2a436ef807ae14510b8986a85f21ccff2f3aed72
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2024-09-23 21:05:19 +0300

    add config for typescript compilation

---
+++ dist/constants/keys.d.ts
@@ -0,0 +1,11 @@
+export declare const KEYS: {
+    END: string;
+    HOME: string;
+    LEFT: string;
+    UP: string;
+    RIGHT: string;
+    DOWN: string;
+    DELETE: string;
+    ENTER: string;
+    SPACE: string;
+};
